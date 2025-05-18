81e063b3bac20fd433b23f034b7d167762e98b5d
pankaj pundir <pankaj.pundir369@gmail.com>
2025-05-18 07:43:51 +0000

    updated the cortex bot project

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -10,10 +10,10 @@ export const userInfo = {
     github: 'https://github.com/pankaj-pundir',
     linkedin: 'https://www.linkedin.com/in/pankaj-pundir',
     twitter: 'https://www.twitter.com/twt_curious_guy',
-    facebook: 'https://www.facebook.com/pankaj.pundir.92',
-    skype: 'skype:pankaj.pundir369?chat',
+    // facebook: 'https://www.facebook.com/pankaj.pundir.92',
+    // skype: 'skype:pankaj.pundir369?chat',
     email: 'pankaj.pundir369@gmail.com',
-    medium: '', // Add medium link if available
+    // medium: '', // Add medium link if available
   },
   profileImage: "/images/profilepic.jpg", 
 };
@@ -25,9 +25,9 @@ export const projects: Project[] = [
     subheading: 'Manage Stocks Wisely',
     description: 'Algo Trading platform to provide valuable trade suggestions using Technical analysis and Machine learning techniques. This is an ongoing long-term project (2+ years). Key features include: Place Trade via a broker, Backtest your strategy [5yrs data], Analysis Tools and visualization, Trade suggestions via WhatsApp.',
     imageUrls: [
-      'https://placehold.co/600x400.png', 
-      'https://placehold.co/600x400.png', 
-      'https://placehold.co/600x400.png'  
+      '/images/cortexbot/generateSignals.png', 
+      '/images/cortexbot/signalsDashboard.png',  
+      '/images/cortexbot/strategyDashboard.png',  
     ], 
     tags: ['React', 'FastAPI', 'MongoDB', 'PostgreSQL', 'Redis', 'Google Cloud', 'Microservices', 'Algo Trading', 'Machine Learning', 'Technical Analysis'],
     liveLink: '#', 
@@ -37,7 +37,7 @@ export const projects: Project[] = [
     id: '1',
     title: 'ColorTuner',
     description: 'This software is made to visualize and extract the colors from the images with exact HSV coding. Thus very useful for color extraction, and color detection tuning.',
-    imageUrl: '/images/portfolio/colorTuner.gif',
+    imageUrl: '/images/colorTuner.gif',
     videoUrl: '',
     tags: ['Kivy', 'Python', 'Image Processing', 'App design', 'Color Extractor'],
     liveLink: '#',
@@ -45,9 +45,10 @@ export const projects: Project[] = [
   },
   {
     id: '2',
-    title: 'Graph Reader (FigSense)',
+    title: 'Graph Reader',
+    subheading: 'FigSense',
     description: 'A fully automated GUI to read graphical data like bar and line chart and extract the information for the user. Line and bar charts can be extracted from images as well as pdf and classifying the images in categories. Finally extracting points, line, and textual data. Merging all the information and provide meaningful data to the user.',
-    imageUrl: '/images/portfolio/graphReader.jpg',
+    imageUrl: '/images/graphReader.jpg',
     tags: ['Kivy', 'OpenCV', 'Computer Vision', 'Python'],
     liveLink: '#',
     sourceLink: '#',
@@ -56,7 +57,7 @@ export const projects: Project[] = [
     id: '3',
     title: 'Dexterous',
     description: 'Software to replace GPS. It track position of object with accelerometer and gyroscope, thus mapping 3D object poesition.',
-    imageUrl: '/images/portfolio/dexterous.jpg',
+    imageUrl: '/images/dexterous.jpg',
     tags: ['matplotlib', 'sensors', 'Python', '3D Tracking'],
     liveLink: '#',
     sourceLink: '#',
@@ -65,7 +66,7 @@ export const projects: Project[] = [
     id: '4',
     title: 'Instant Attendance',
     description: 'To decrease the attendance time in college. OTP method is implemented with sharing a secret code with the students to mark the attendance parallelly. This process decreases the time as compared to serial attendance.',
-    imageUrl: '/images/portfolio/attendance.gif',
+    imageUrl: '/images/attendance.gif',
     tags: ['PHP', 'CSS', 'JavaScript', 'Web Development'],
     liveLink: '#',
     sourceLink: '#',
@@ -88,7 +89,7 @@ export const workHistory: WorkItem[] = [
   {
     id: 'google-swe3',
     company: 'Google',
-    companyLogoUrl: 'https://placehold.co/100x40.png',
+    companyLogoUrl: 'https://media.licdn.com/dms/image/v2/C4D0BAQHiNSL4Or29cg/company-logo_200_200/company-logo_200_200/0/1631311446380?e=1752710400&v=beta&t=_zAJJHkDhi7EvfyEGIEBojzv4ci4mmIAm6I_5Xqnua8',
     role: 'Software Engineer 3 (L4)',
     duration: 'June 2024 - Present',
     description: [
@@ -100,7 +101,7 @@ export const workHistory: WorkItem[] = [
   {
     id: 'phonepe-sde2',
     company: 'PhonePe',
-    companyLogoUrl: 'https://placehold.co/100x40.png',
+    companyLogoUrl: 'https://media.licdn.com/dms/image/v2/D560BAQHLOrShxWW33g/company-logo_200_200/company-logo_200_200/0/1732870614932/phonepe_internet_logo?e=1752710400&v=beta&t=z4qwuh1wuNPC1V7aVbhwzcLwXJ2rir_w55jsArglG9Y',
     role: 'SDE 2',
     duration: 'April 2024 - May 2024',
     description: [
@@ -112,7 +113,7 @@ export const workHistory: WorkItem[] = [
   {
     id: 'groww-sde2',
     company: 'GROWW',
-    companyLogoUrl: 'https://placehold.co/100x40.png',
+    companyLogoUrl: 'https://resources.groww.in/web-assets/img/website-logo/groww-logo-light.svg',
     role: 'Software Engineer 2 (Credit Team)',
     duration: 'Feb 2022 - March 2024 | Banglore, India',
     description: [
@@ -126,7 +127,7 @@ export const workHistory: WorkItem[] = [
   {
     id: 'bnym-swe',
     company: 'BANK OF NEW YORK MELLON',
-    companyLogoUrl: 'https://placehold.co/100x40.png',
+    companyLogoUrl: 'https://nexen.bnymellon.com/app/shared-wl-assets/17/img/whitelabel/mmi/logo_bny.png',
     role: 'Software Engineer',
     duration: 'July 2020 - Feb 2022 | Pune, India',
     description: [
@@ -140,7 +141,7 @@ export const workHistory: WorkItem[] = [
   {
     id: 'bnym-intern',
     company: 'BANK OF NEW YORK MELLON',
-    companyLogoUrl: 'https://placehold.co/100x40.png',
+    companyLogoUrl: 'https://nexen.bnymellon.com/app/shared-wl-assets/17/img/whitelabel/mmi/logo_bny.png',
     role: 'Software Engineer Intern',
     duration: 'Jan - June 2020 | Pune, India',
     description: [
@@ -153,7 +154,7 @@ export const workHistory: WorkItem[] = [
   {
     id: 'iitrpr-intern1',
     company: 'IIT ROPAR',
-    companyLogoUrl: 'https://placehold.co/100x40.png',
+    companyLogoUrl: 'http://www.iitrpr.ac.in/sites/default/files/image.jpg',
     role: 'Research Intern',
     duration: 'May 2019 – Jul 2019 | Punjab, India',
     description: [
@@ -166,7 +167,7 @@ export const workHistory: WorkItem[] = [
   {
     id: 'iitrpr-intern2',
     company: 'IIT ROPAR',
-    companyLogoUrl: 'https://placehold.co/100x40.png',
+    companyLogoUrl: 'http://www.iitrpr.ac.in/sites/default/files/image.jpg',
     role: 'Research Intern',
     duration: 'May 2018 – Jul 2018 | Punjab, India',
     description: [
